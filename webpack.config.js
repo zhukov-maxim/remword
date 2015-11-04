7e9fc4e6db1524df55a59bf6ab61f041154a5ed0
Maxim Zhukov <zhkv.mxm@gmail.com>
2015-11-04 23:43:49 +0600

    Update linting

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ var config = {
       },
       {
         test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         exclude: /node_modules/
       }
     ]
